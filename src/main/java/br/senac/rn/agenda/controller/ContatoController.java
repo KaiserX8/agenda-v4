2b62883dc46b1e1139211376f1296acbbc05ebc8
KaiserX8 <a9244218@rn.senac.br>
2019-09-19 21:12:58 -0300

    first commit

---
+++ src/main/java/br/senac/rn/agenda/controller/ContatoController.java
@@ -0,0 +1,36 @@
+package br.senac.rn.agenda.controller;
+
+import br.senac.rn.agenda.model.Contato;
+import br.senac.rn.agenda.service.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/contatos")
+public class ContatoController {
+
+    @Autowired
+    private ContatoService service;
+
+    @GetMapping
+    public String listaTodos(Model model) {
+        Contato contato = new Contato();
+        model.addAttribute("contato", contato);
+        List<Contato> contatos = service.listaTodos();
+        model.addAttribute("contatos", contatos);
+        return "contatos";
+    }
+
+    @PostMapping
+    public String salva(Contato contato) {
+        service.salva(contato);
+        return "redirect:/contatos";
+    }
+
+}
