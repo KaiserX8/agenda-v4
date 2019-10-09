a495d0749dd02e7367c35066e767ce0250fa78db
KaiserX8 <a9244218@rn.senac.br>
2019-10-09 19:16:34 -0300

    adicional

--- src/main/java/br/senac/rn/agenda/model/Contato.java
+++ src/main/java/br/senac/rn/agenda/model/Contato.java
@@ -1,8 +1,15 @@
 package br.senac.rn.agenda.model;
 
-import javax.persistence.*;
-import java.util.Objects;
+import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
 @Entity
 @Table(name = "tb_contatos")
 public class Contato {
@@ -18,40 +25,4 @@ public class Contato {
     @Column(name = "con_fone", nullable = false)
     private String fone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getFone() {
-        return fone;
-    }
-
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contato contato = (Contato) o;
-        return id.equals(contato.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
